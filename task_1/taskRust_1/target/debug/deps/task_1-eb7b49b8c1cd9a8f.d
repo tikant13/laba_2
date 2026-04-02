261f466f8d8c9c263d0850e8823bd318272cb421
tikant13 <tikant13@gmail.com>
2026-04-02 19:21:14 +0700

    Решено

---
+++ task_1/taskRust_1/target/debug/deps/task_1-eb7b49b8c1cd9a8f.d
@@ -0,0 +1,5 @@
+/home/kolmakov/Projects/laba_2/task_1/taskRust_1/target/debug/deps/task_1-eb7b49b8c1cd9a8f.d: src/main.rs
+
+/home/kolmakov/Projects/laba_2/task_1/taskRust_1/target/debug/deps/libtask_1-eb7b49b8c1cd9a8f.rmeta: src/main.rs
+
+src/main.rs:
